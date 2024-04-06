686278b4dbda8582e3265d78ef9422cd3a7b3b4a
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-06 08:21:51 +0300

    fetch request

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")// fetch the data
+  .then(res => res.json())// then translate the data
+  .then((json) => renderBooks(json));// then render the data 
 }
+ 
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
